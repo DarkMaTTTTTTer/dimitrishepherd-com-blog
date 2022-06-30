38207cc13313e4cb4b309369ebc48770964bb619
DarkMaTTTTTTer <61645374+DarkMaTTTTTTer@users.noreply.github.com>
2022-06-29 21:41:32 -0700

    v3

---
+++ src/components/BlogHeader.vue
@@ -0,0 +1,16 @@
+<template>
+  <div class="text-center my-5">
+    <router-link
+      :to="'/'"
+      class="text-reset"
+    >
+      <p class="display-3">
+        Dimitri's Blog
+      </p>
+      <img
+        alt="logo"
+        src="@/assets/logo.png"
+      >
+    </router-link>
+  </div>
+</template>
